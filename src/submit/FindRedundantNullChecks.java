404ac28f0e8ac00c84a4d379f48671943ad43a4a
chaojun xiao <xcj@chaojundeMacBook-Pro.local>
2019-12-14 14:42:11 +0800

    add the NullAnalysis class

--- src/submit/FindRedundantNullChecks.java
+++ src/submit/FindRedundantNullChecks.java
@@ -17,20 +17,17 @@ public class FindRedundantNullChecks {
      */
     public static void main(String[] args) {
         // TODO: Fill in this, like:
-        List<String> args_ = new ArrayList<String>(Arrays.asList(args));
         Flow.Solver solver = new FlowSolver();
         
         // Flow.Analysis analysis = new NonNull(NonNull.LEVEL_NORMAL);
 
-        // jq_Class[] classes = new jq_Class[args.size()];
-        // for (int i = 0; i < classes.length; i++)
-        //     classes[i] = (jq_Class) Helper.load(args.get(i));
-
-        for (String name : args) {
-            jq_Class clazz = (jq_Class) Helper.load(name);
-            Analysis analysis = new PrintRedundant();
-            solver.registerAnalysis(analysis);
-            Helper.runPass(clazz, solver);
+        jq_Class[] classes = new jq_Class[args.size()];
+        for (int i = 0; i < classes.length; i++)
+            classes[i] = (jq_Class) Helper.load(args.get(i));
+        
+            solver.registerAnalysis(new NullAnalysis());
+        for (int i = 0; i < classes.length; i++) {
+            Helper.runPass(classes[i], solver);
         }
         
     }
